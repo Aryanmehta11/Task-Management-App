f49f8711c6b0c13ffdaaea88fc8d92bbf1346478
am499702@gmail.com <am499702@gmail.com>
2024-12-17 12:27:07 +0530

    Import Changes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from backend.extensions import db
-from routes import routes
+from backend.routes import routes
 
 app = Flask(__name__)
 CORS(app)
