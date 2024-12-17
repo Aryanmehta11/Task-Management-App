6a87cb229e6ad1e65e438ea1f90b995b45dee1d4
am499702@gmail.com <am499702@gmail.com>
2024-12-17 11:30:12 +0530

    Setup backend with Flask and added required files

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////data/tasks.db' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize database
